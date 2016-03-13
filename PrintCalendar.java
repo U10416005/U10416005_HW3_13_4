041fa88cb25c3ca7e69775d51be11bd89e251bcf
U10416005 <r3r346@gmail.com>
2016-03-13 15:21:12 +0800

    Write form
    Use System.out.print write the form of Calendar

--- PrintCalendar.java
+++ PrintCalendar.java
@@ -7,5 +7,8 @@ public class PrintCalendar{
 	}
 	public void mCalendar{
 		GregorianCalendar g = new GregorianCalendar();
+		System.out.println("          " + g.get(GregorianCalendar.MONTH) + ",  " + g.get(GregorianCalendar.YEAR));
+		System.out.println("---------------------------------------------------");
+		System.out.println("日\t一\t二\t三\t四\t五\t六");
 	}
 }
