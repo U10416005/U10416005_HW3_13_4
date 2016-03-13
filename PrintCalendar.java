e6df33613c9178c5883d6645b260c8805906ac2a
U10416005 <r3r346@gmail.com>
2016-03-13 19:10:35 +0800

    Write while for the Calendar date
    Use while to show the Calendar date and  int week,month and day for using

--- PrintCalendar.java
+++ PrintCalendar.java
@@ -8,10 +8,18 @@ public class PrintCalendar{
 	}
 	public void mCalendar(){
 		GregorianCalendar g = new GregorianCalendar();
-		System.out.println("          " + g.get(GregorianCalendar.MONTH) + ",  " + g.get(GregorianCalendar.YEAR));
+		System.out.println("                   " + g.get(GregorianCalendar.MONTH) + ",  " + g.get(GregorianCalendar.YEAR));
 		System.out.println("---------------------------------------------------");
 		System.out.println("日\t一\t二\t三\t四\t五\t六");
 		g.set(Calendar.DAY_OF_MONTH, 1);
 		int week = g.get(Calendar.DAY_OF_WEEK);
+		int month = g.get(Calendar.MONTH);
+		for (int i = Calendar.SUNDAY; i < week; i++) {
+		System.out.print("\t");
+		}
+		while (g.get(Calendar.MONTH) == month) {
+			int day = g.get(Calendar.DAY_OF_MONTH);
+				System.out.print("" + day + "\t");
+		}
 	}
 }
