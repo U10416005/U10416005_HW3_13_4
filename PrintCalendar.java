0e94d40869e6b6c4eb76207a5540360730d5c63f
U10416005 <r3r346@gmail.com>
2016-03-13 17:19:29 +0800

    invoke mCalendar in main method
    invoke mCalendar in main method to test the result

--- PrintCalendar.java
+++ PrintCalendar.java
@@ -3,12 +3,15 @@ import java.util.*;
 
 public class PrintCalendar{
 	public static void main(String[] args){
-		
+		PrintCalendar og = new PrintCalendar();
+		og.mCalendar();
 	}
-	public void mCalendar{
+	public void mCalendar(){
 		GregorianCalendar g = new GregorianCalendar();
 		System.out.println("          " + g.get(GregorianCalendar.MONTH) + ",  " + g.get(GregorianCalendar.YEAR));
 		System.out.println("---------------------------------------------------");
 		System.out.println("日\t一\t二\t三\t四\t五\t六");
+		g.set(Calendar.DAY_OF_MONTH, 1);
+		int week = g.get(Calendar.DAY_OF_WEEK);
 	}
 }
